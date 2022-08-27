bae46f9a22527c24fd9b20474ef3fe5b28481b6c
Chuang, Jason <jc1257@ruckuswireless.com>
2022-08-27 12:34:26 +0800

    3td

--- CloudEngineering/CloudIAM:QwikStart/replace_para.sh
+++ CloudEngineering/CloudIAM:QwikStart/replace_para.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i 's/kumo-cicd/your_project/g' 1/main.tf
-sed -i 's/jane@example.com"/your_student_email/g' 2/main.tf
+sed -i -e 's/kumo-cicd/your_project/g' 1/main.tf
+sed -i -e 's/jane@example.com/your_student_email/g' 2/main.tf
