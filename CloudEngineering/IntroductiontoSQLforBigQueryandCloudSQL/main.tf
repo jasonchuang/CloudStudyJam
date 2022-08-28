1ecec0dd436d9f9dd182d72ee0457f64cc29b75d
james12695 <james12695.chuang@gmail.com>
2022-08-28 15:14:49 +0800

    bq cloud sql

---
+++ CloudEngineering/IntroductiontoSQLforBigQueryandCloudSQL/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.33.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.3.2"
+    }
+
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project     = "kumo-cicd"
+  region      = "us-central1"
+}
+
+resource "google_sql_database_instance" "main" {
+  name             = "qwiklabs-demo"
+  database_version = "MYSQL_5_7"
+  region           = "us-central1"
+  password         = "mypassword"
+
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-f1-micro"
+  }
+}
+
+resource "random_string" "random_bucket_name" {
+  length           = 12
+  upper            = false
+  special          = false
+}
+
+output "random_bucket_string" {
+  value = random_string.random_bucket_name.result
+}
+
+
+resource "google_storage_bucket" "static-site" {
+  name          = random_string.random_bucket_name.result
+  location      = "US"
+}
+
+resource "google_storage_bucket_object" "bq_csv" {
+  name          = "bq.csv"
+  source        = "./bq.csv"
+  bucket        = google_storage_bucket.static-site.name
+}
+
