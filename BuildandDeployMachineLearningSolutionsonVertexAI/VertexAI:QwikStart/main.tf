1abc591660c5db034f2bb768a2c37378917932cf
Chuang, Jason <jc1257@ruckuswireless.com>
2022-08-31 20:05:23 +0800

    vertexai

---
+++ BuildandDeployMachineLearningSolutionsonVertexAI/VertexAI:QwikStart/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.33.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.3.2"
+    }
+
+  }
+}
+
+provider "google" {
+  project     = "${var.gcp_project}"
+  region      = "${var.gcp_region}"
+}
+
+
+resource "google_notebooks_instance" "instance" {
+  name = "notebooks-instance"
+  location = "us-west1-a"
+  machine_type = "e2-medium"
+
+  vm_image {
+    project      = "deeplearning-platform-release"
+    image_family = "tf-ent-2-3-cpu"
+  }
+}
+
+
+
+
