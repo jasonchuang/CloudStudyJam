070bf471707ad7a5bb27eeec8977705894ad5b2c
Chuang, Jason <jc1257@ruckuswireless.com>
2022-08-31 20:22:02 +0800

    Cloud Monitoring

--- CloudEngineering/CloudMonitoring:QwikStart/main.tf
+++ CloudEngineering/CloudMonitoring:QwikStart/main.tf
@@ -17,14 +17,23 @@ variable "gcp_project" {
   description = "GCP Project."
 }
 
-
 variable "gcp_zone" {
   type        = string
   description = "GCP Zone."
 }
 
+variable "gcp_region" {
+  type        = string
+  description = "GCP Region."
+}
+
+provider "google" {
+  # Configuration options
+  project     = "${var.gcp_project}"
+  region      = "${var.gcp_region}"
+}
+
 resource "google_compute_instance" "lamp-1-vm" {
-  project      = "${var.gcp_project}"
   name         = "lamp-1-vm"
   boot_disk {
     initialize_params {
@@ -50,113 +59,3 @@ resource "google_compute_instance" "lamp-1-vm" {
     SCRIPT
 }
 
-resource "google_monitoring_uptime_check_config" "http" {
-  display_name = "Lamp Uptime Check, then click Next"
-  period       = "60s"
-  timeout      = "60s"
-
-  http_check {
-    path = ""
-  }
-
-  #  http_check {
-  #    port = "8010"
-  #    request_method = "POST"
-  #    content_type = "URL_ENCODED"
-  #    body = "Zm9vJTI1M0RiYXI="
-  #  }
-
-  monitored_resource {
-    type = "gce_instance"
-    labels = {
-      project_id   = "${var.gcp_project}"
-      instance_id  = google_compute_instance.lamp-1-vm.instance_id
-      zone         = "${var.gcp_zone}"
-    }
-  }
-
-  #  checker_type = "STATIC_IP_CHECKERS"
-}
-
-resource "google_monitoring_notification_channel" "basic_notify" {
-  display_name = "notify_jason"
-  type         = "email"
-  labels = {
-    email_address = "chunchieh.chuang@gmail.com"
-  }
-}
-
-resource "google_monitoring_alert_policy" "alert_policy" {
-  display_name = "Inbound Traffic Alert"
-
-  #  Network traffic
-  #  Set the Threshold position to Above threshold, Threshold value to 500 and Advanced Options > Retest window to 1 min. Click Next.
-  #  oClick on Notification Channels again, then click on the Refresh icon to get the display name you mentioned in the previous step.
-  #Click on Notification Channels again if necessary, select your Display name and click OK.
-  #
-  #
-  combiner     = "OR"
-  conditions {
-    display_name = "testcondition"
-    condition_threshold {
-      filter     = "metric.type=\"agent.googleapis.com/interface/traffic\""
-      duration   = "500s"
-      comparison = "COMPARISON_GT"
-      aggregations {
-        alignment_period   = "60s"
-        per_series_aligner = "ALIGN_RATE"
-      }
-    }
-  }
-}
-
-#
-#Type CPU load (1m) in filter by resource and metric name and click on VM instance > Cpu.
-# Select CPU load (1m) and click Apply. Leave all other fields at the default value. Refresh the tab to view the graph.
-#Add the second chart
-#Click + Add Chart and select Line option in Chart library.
-#Name this chart Received Packets.
-#Click on Resource & Metric dropdown. Disable the Show only active resources & metrics.
-#Type Received packets in filter by resource and metric name and click on VM instance > Instance. Select Received packets and click Apply. Refresh the tab to view the graph.
-#
-#resource "google_monitoring_dashboard" "dashboard" {
-#  dashboard_json = <<EOF
-#  {
-#  "displayName": "Grid Layout Example",
-#  "gridLayout": {
-#    "columns": "2",
-#    "widgets": [
-#      {
-#        "title": "Widget 1",
-#        "xyChart": {
-#          "dataSets": [{
-#            "timeSeriesQuery": {
-#              "timeSeriesFilter": {
-#                "filter": "metric.type=\"agent.googleapis.com/nginx/connections/accepted_count\"",
-#                "aggregation": {
-#                  "perSeriesAligner": "ALIGN_RATE"
-#                }
-#              },
-#              "unitOverride": "1"
-#            },
-#            "plotType": "LINE"
-#          }],
-#          "timeshiftDuration": "0s",
-#          "yAxis": {
-#            "label": "y1Axis",
-#            "scale": "LINEAR"
-#          }
-#        }
-#      },
-#      {
-#        "text": {
-#          "content": "Widget 2",
-#          "format": "MARKDOWN"
-#        }
-#      }
-#    ]
-#  }
-#}
-#EOF
-#}
-#
